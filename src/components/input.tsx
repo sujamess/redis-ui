766fb0ef492911e8870a9274d9fdbb7869b62aca
sujamess <surarerk.boonjit@gmail.com>
2022-02-02 20:30:17 +0700

    feat: add id prop to input component

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,6 +1,7 @@
 import { ChangeEvent, FunctionComponent } from "react";
 
 interface Input {
+  id: string;
   type: 'text' | 'email' | 'password' | 'number';
   label: string;
   placeholder?: string;
@@ -11,13 +12,13 @@ interface Input {
   onChange: (e: ChangeEvent<HTMLInputElement>) => void;
 }
 
-const Input: FunctionComponent<Input> = ({ type, label, placeholder, required, min, max, value, onChange }: Input) => {
+const Input: FunctionComponent<Input> = ({ id, type, label, placeholder, required, min, max, value, onChange }) => {
   return (
     <div>
-      <label htmlFor={label.toLowerCase()} className="block text-sm font-medium text-gray-900 dark:text-gray-300">{label}</label>
+      <label htmlFor={id} className="block text-sm font-medium text-gray-900 dark:text-gray-300">{label}</label>
       <input
         type={type}
-        id={label.toLowerCase()}
+        id={id}
         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
         placeholder={placeholder}
         required={required}
